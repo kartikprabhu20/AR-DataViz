b2d0d7c6e8d64c1f5dde598035d1160e85dba6d2
kartikprabhu20 <kartikprabhu20@yahoo.co.in>
2020-01-19 09:54:52 +0100

    commit after changes

---
+++ ARPlot/Assets/ClusterSettingsManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClusterSettingsManager : MonoBehaviour
+{
+
+    public MeshFilter square;
+    public MeshFilter sphere;
+    public MeshFilter diamond;
+
+    string clusterName;
+    DataReader dataReader;
+    GameObject pointsHolder;
+    List<int> list;
+
+    void Start()
+    {
+        clusterName = GetClusterName();
+        Debug.Log("In the cluster 1 " + clusterName);
+
+    }
+
+    public void init(ScatterplotGenerator scatterplotGenerator)
+    {
+        clusterName = GetClusterName();
+        Debug.Log("In the cluster " + clusterName);
+
+        pointsHolder = GameObject.Find("PointsHolder");
+        dataReader = scatterplotGenerator.GetComponent<DataReader>();
+
+        list = dataReader.ClusterMap[clusterName];
+    }
+
+    public void ToggleClusterActivation(Toggle toggle)
+    {
+        foreach (int i in list)
+        {
+            pointsHolder.transform.GetChild(i).gameObject.SetActive(toggle.isOn);
+        }
+    }
+
+
+    string GetClusterName()
+    {
+        //Debug.Log("GetClusterName " + gameObject.transform.GetChild(0).GetChild(1).GetComponent<Text>().text);
+        return gameObject.transform.GetChild(0).GetChild(1).GetComponent<Text>().text;
+    }
+
+    public void OnClickSquare()
+    {
+        Debug.Log("OnClickSquare " + GetClusterName() + ":" + list.Count);
+
+        foreach (int i in list)
+        {
+            pointsHolder.transform.GetChild(i).GetComponent<MeshFilter>().sharedMesh = square.sharedMesh;
+        }
+    }
+
+    public void OnClickCircle()
+    {
+        Debug.Log("OnClickCircle " + GetClusterName()+ ":" + list.Count);
+        foreach (int i in list)
+        {
+            pointsHolder.transform.GetChild(i).GetComponent<MeshFilter>().sharedMesh = sphere.sharedMesh;
+        }
+    }
+
+  
+
+    public void OnClickDiamond()
+    {
+        Debug.Log("OnClickDiamond " + GetClusterName() + ":" + list.Count);
+
+        foreach (int i in list)
+        {
+            pointsHolder.transform.GetChild(i).GetComponent<MeshFilter>().sharedMesh = diamond.sharedMesh;
+        }
+    }
+
+}
---
+++ ARPlot/Assets/Scripts/IPlotLoad.cs
@@ -0,0 +1,5 @@
+﻿
+interface IPlotLoad
+{
+    void callback();
+}
